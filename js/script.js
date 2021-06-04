13a662f1fdb69323625e95a0b7f8799ad0afa8c1
luigi2001 <luigitanga@virgilio.it>
2021-06-04 17:00:09 +0200

    add input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 
+var invia = document.getElementById('btn_invia');
+
+var annulla = document.getElementById('btn_annulla');
+
+invia.addEventListener('click', function() {
+
+    document.getElementById('biglietto').className = 'show';
+
+    /* input */
+
+    var nome = document.getElementById('nome_cognome').value;
+    console.log(nome);
+
+    var km = document.getElementById('Km').value;
+    console.log(km);
+
+    var eta = document.getElementById('eta').value;
+    console.log(eta);
+
+    /* calcolo prezzo biglietto */
+
+    var prezzo = 
+
+
+})
